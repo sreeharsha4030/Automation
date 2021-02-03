4e8a10beeac7a2e5f970e979ead82ffa067ca56d
sreeharsha4030 <donepudiharsha89@gmail.com>
2021-02-02 18:42:44 -0700

    updating the files

--- videoshare.py
+++ videoshare.py
@@ -1,18 +1,35 @@
 import pyautogui
-import time
+from time import sleep
 
+def videoShare():
 #search for video file
-pyautogui.press('esc', interval=0.1)
-time.sleep(0.2)
+    pyautogui.press('esc', interval=0.1)
+    sleep(0.2)
 
-pyautogui.press('win', interval=0.1)
-pyautogui.write('ks_1920')
-time.sleep(5)
+    pyautogui.press('win', interval=0.1)
+    sleep(5)
+    pyautogui.write('ks_1920')
+    sleep(5)
 
-pyautogui.press('enter', interval=0.5)
-time.sleep(5)
+    pyautogui.press('enter', interval=0.5)
+    sleep(5)
+    
+    pyautogui.hotkey('win', 'up')
 
-#select the repeat mode.
-pyautogui.hotkey('ctrl', 'T')
-time.sleep(2)
+    #select the repeat mode.
+    pyautogui.hotkey('ctrl', 'T')
+    sleep(2)
+    
+    #making it full screen
+    #pyautogui.moveTo(1058, 467)
+    pyautogui.doubleClick(1058, 467)
+    sleep(2)
+    pyautogui.doubleClick(1058, 467)
 
+def main():
+    print("Starting the video share")
+    playingVideo = videoShare()
+    
+
+if __name__ == "__main__":
+    main()
